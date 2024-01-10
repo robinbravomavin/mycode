6cd68d8a8f99ea1a745281c4ce56ca13b665dc48
robinbravomavin <robinbmavins@gmail.com>
2024-01-10 17:53:03 +0700

    returning back Obsidian Tutorial Page

--- flutter/startfromscratch_app/lib/main.dart
+++ flutter/startfromscratch_app/lib/main.dart
@@ -1,25 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'package:startfromscratch_app/gradient_container.dart';
+
 void main() {
-  runApp(MaterialApp(
+  runApp(
+    const MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(colors: [
-              Color.fromARGB(255, 0, 2, 31),
-              Color.fromARGB(255, 43, 38, 122),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text('Hello World!', style: TextStyle(
-              color: Colors.white,
-              fontSize: 28,
-              ),
-              ),
-          ),
+        body: GradientContainer(
+          Color.fromARGB(255, 9, 0, 55),
+          Color.fromARGB(255, 39, 20, 161),
         ),
       ),
     ),
