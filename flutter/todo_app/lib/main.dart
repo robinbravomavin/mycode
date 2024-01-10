fe60bbc378092df032bbe82e1cc7f0f7b87ab22f
robinbravomavin <robinbmavins@gmail.com>
2024-01-10 23:56:12 +0700

    creating to do app

---
+++ flutter/todo_app/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:todo_app/screens/home.dart';
+
+void main() {
+runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'To Do App',
+      home: Home(),
+      );
+  }
+}
